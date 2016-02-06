95322658bccb53e3c3413d17c487954a39729a42
Steve Haight <stevebhaight@gmail.com>
2016-02-05 19:12:06 -0700

    added .gitignore

--- public/javascripts/app/modules/core/coreModule.js
+++ public/javascripts/app/modules/core/coreModule.js
@@ -6,8 +6,8 @@ define(['core/runners/logRunner'], function(logRunner) {
 	
 	coreModule.config(['$routeProvider', function($routeProvider){
 		$routeProvider
-			.when('/', { controller: 'homeController', templateUrl: '/app/modules/core/views/home.html' })
-			.when('/home', { controller: 'homeController', templateUrl: '/app/modules/core/views/home.html' })
+			.when('/', { controller: 'homeController', templateUrl: '/views/home.html' })
+			.when('/home', { controller: 'homeController', templateUrl: '/views/home.html' })
 	}]);
 	
 	require(['core/controllerReferences'], function(references) {
