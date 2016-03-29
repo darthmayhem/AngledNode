fd88f1c2e1d649bac79cc21ec99fd3c0649c9f8d
Steve Haight <stevebhaight@gmail.com>
2016-03-29 11:03:53 -0600

    [SH] renamed bin\www file to include extention

--- bin/config.js
+++ bin/config.js
@@ -7,7 +7,8 @@ var config = {
     "version": "0.1.0",
     "appKey": "00000000",
     "resources": {
-        "db_uri": "mongodb://localhost:27017/angledNode"
+        //"db_uri": "mongodb://localhost:27017/angledNode"
+        "db_uri": "mongodb://anglednode:D4rth-mayhem@ds059215.mlab.com:59215/anglednode"
     }
 };
 
